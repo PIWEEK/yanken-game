709573652ac96ac682621dd10c6483ba6f18b3bb
alonso.torres <alonso.torres@kaleidos.net>
2021-12-16 23:40:45 +0100

    deploy

--- _app/chunks/navigation-d8ceaada.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./paths-c3a241ee.js";const s=r,a=e;async function e(o,t){return s.goto(o,t,[])}export{a as g};
--- _app/chunks/paths-c3a241ee.js
+++
@@ -1,1 +0,0 @@
-let e;function a(s){e=s}let t="";function i(s){t=s.base,s.assets}export{t as b,a as i,e as r,i as s};

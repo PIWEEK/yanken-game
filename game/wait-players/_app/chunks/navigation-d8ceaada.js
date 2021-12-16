5eb433417ad88b30ce94ca8a9242531ad42ed86f
alonso.torres <alonso.torres@kaleidos.net>
2021-12-16 14:56:21 +0100

    deploy

---
+++ game/wait-players/_app/chunks/navigation-d8ceaada.js
@@ -0,0 +1,1 @@
+import{r}from"./paths-c3a241ee.js";const s=r,a=e;async function e(o,t){return s.goto(o,t,[])}export{a as g};
---
+++ game/wait-players/_app/chunks/paths-c3a241ee.js
@@ -0,0 +1,1 @@
+let e;function a(s){e=s}let t="";function i(s){t=s.base,s.assets}export{t as b,a as i,e as r,i as s};
